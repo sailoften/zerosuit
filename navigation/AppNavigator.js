4667e9b27c3c1d4a1b0f2a389ca9dfd9d4af3127
Jimmy Liu <sicong.liu98@gmail.com>
2019-10-15 15:14:25 -0700

    Updates

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createAppContainer, createStackNavigator, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import MainTabNavigator from './MainTabNavigator';
 import LoginScreen from '../screens/Login';
